a6031b7a2f66fe9b6363b3febbdc2aded925f78f
Sam Der <sder@uci.edu>
2023-09-25 00:33:26 -0700

    Added meeting times

--- apps/web/components/Header/Header.tsx
+++ apps/web/components/Header/Header.tsx
@@ -1,15 +1,21 @@
+import { PropsWithChildren } from "react";
 import styles from "./Header.module.scss";
 
 interface HeaderProps {
-  headerText: string;
-  subHeaderText: string;
+    headerText: string;
+    subHeaderText: string;
 }
 
-export default function Header({ headerText, subHeaderText }: HeaderProps) {
-  return (
-    <div className={styles.header}>
-      <h1>{headerText}</h1>
-      <p>{subHeaderText}</p>
-    </div>
-  );
+export default function Header({
+    headerText,
+    subHeaderText,
+    children,
+}: PropsWithChildren<HeaderProps>) {
+    return (
+        <div className={styles.header}>
+            <h1>{headerText}</h1>
+            <p>{subHeaderText}</p>
+            {children}
+        </div>
+    );
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,18 +1,23 @@
-import Head from 'next/head';
+import Head from "next/head";
 import About from "../components/About/About";
 import Header from "../components/Header/Header";
 
 export default function Landing() {
-  return (
-    <>
-      <Head>
-        <title>Home | ACM @ UCI</title>
-      </Head>
-      <Header
-        headerText="ACM @ UCI"
-        subHeaderText="Learn about algorithms and improve your interview skills"
-      />
-      <About />
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Home | ACM @ UCI</title>
+            </Head>
+            <Header
+                headerText="ACM @ UCI"
+                subHeaderText="Learn about algorithms and improve your interview skills"
+            >
+                <strong>
+                    Meetings will be on Mondays and Wednesdays from 6:30pm to
+                    8:30pm at DBH 4011!
+                </strong>
+            </Header>
+            <About />
+        </>
+    );
 }
