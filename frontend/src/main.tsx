e6ca631a584863679db389c456073aae3c637c87
Citralyn <alexaduffy1414@gmail.com>
2025-10-08 12:10:41 -0700

    added Vercel web analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from "react-router";
+import { Analytics } from '@vercel/analytics/next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.tsx'
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HashRouter>
       <App />
+      <Analytics />
     </HashRouter>
   </StrictMode>,
 )
